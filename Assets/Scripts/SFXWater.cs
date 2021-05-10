e9e35aaa6701824dfe1744437028ca8119748670
paulinalasz <laszpaulina@gmail.com>
2021-05-10 13:25:24 +0100

    made sfx water work for now, altho doesnt inherit

--- Assets/Scripts/SFXWater.cs
+++ Assets/Scripts/SFXWater.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using System;
 
-public class SFXWater : SFXTriggerSurface {
+public class SFXWater : MonoBehaviour {
 
-    //[SerializeField] AudioSource initialAudio;
+    [SerializeField] AudioSource initialAudio;
+    [SerializeField] AudioSource movementAudio;
 
-    //private Sound initial;
+    private Sound initial;
+    private Sound movement;
 
     private void Start() {
-        //initialAudio = GetComponent<AudioSource>();
-        //initial = new Sound(initialAudio);
+        initial = new Sound(initialAudio);
+        movement = new Sound(movementAudio);
     }
 
     private void Update() {
-        /*if (!initial.isPlaying()) {
+        if (!initial.isPlaying()) {
             GlobalListener.removeSoundsPlaying(initial);
         }
-        /*if (!movement.isPlaying()) {
+        if (!movement.isPlaying()) {
             GlobalListener.removeSoundsPlaying(movement);
         }
         if (initial.isPlaying()) {
@@ -26,13 +28,24 @@ public class SFXWater : SFXTriggerSurface {
         if (movement.isPlaying()) {
             GlobalListener.updateSoundsPlaying(movement);
         }
-
-        print(initial.File);
-        print(movement.File);*/
     }
 
-    /*private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other) {
         initial.play();
         initial.Origin = other.GetComponent<Transform>().position;
-    }*/
+    }
+
+    protected void OnTriggerStay(Collider other) {
+        if (other.attachedRigidbody.velocity.magnitude > 0.5) {
+            movement.play();
+            movement.Origin = other.GetComponent<Transform>().position;
+        }
+        else if (other.attachedRigidbody.velocity.magnitude < 0.5 && other.attachedRigidbody.velocity.magnitude > 0.01) {
+            movement.stop();
+        }
+    }
+
+    protected void OnTriggerExit(Collider other) {
+        movement.stop();
+    }
 }
